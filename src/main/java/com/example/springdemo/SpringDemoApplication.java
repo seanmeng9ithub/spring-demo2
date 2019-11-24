a7fa8965a491bbd9602dbde692ec1791badc6831
seanmeng9ithub <51937009+seanmeng9ithub@users.noreply.github.com>
2019-11-24 09:59:14 -0500

    Update SpringDemoApplication.java

--- src/main/java/com/example/springdemo/SpringDemoApplication.java
+++ src/main/java/com/example/springdemo/SpringDemoApplication.java
@@ -2,11 +2,13 @@ package com.example.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.lang.*;
 
 @SpringBootApplication
 public class SpringDemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("Start");
         SpringApplication.run(SpringDemoApplication.class, args);
     }
 
